0ca5f3723c73317712c86fbd017449bff482153d
Samuel-van-Gurp <82167906+Samuel-van-Gurp@users.noreply.github.com>
2022-04-03 09:40:40 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,9 @@
 import torch
 import numpy as np
 
+####################################################################################################################
+### Old code ###
+
 def extract_ampl_phase(fft_im):
     # fft_im: size should be bx3xhxwx2
       
@@ -103,3 +106,113 @@ def FDA_source_to_target_np( src_img, trg_img, L=0.1 ):
 
     return src_in_trg
 
+######################################################################################################
+#######################################Homemade functions#############################################
+######################################################################################################
+
+def centered_fft(image):
+    fft = torch.fft.fftshift(torch.fft.fftn(image, dim=(-2, -1)), dim=(-2, -1))
+    
+    return fft
+
+def centered_ifft(fft, s=None):
+    image = torch.fft.ifftn(torch.fft.ifftshift(fft, dim=(-2, -1)), dim=(-2, -1), s=s)
+    
+    return image
+
+
+def shape_low_freq_mutate( amp_src, amp_trg, beta, mask_type = 'circulair'):
+    '''
+    Exchange amplitude of the center of the target image (amp_trg) with that for the 
+    source image (amp_src). Possible shapes 'square', 'circulair' or 'gauss'
+    
+    '''
+    _, _, h, w = amp_src.size()
+    beta_pixel = int((min(h,w)*beta)**2)# specificity the number of pixels in a square as depicted in the paper 
+    
+
+    if mask_type == 'square':
+        b = (  np.floor(np.amin((h,w))*beta)  ).astype(int)     # get b
+         
+        amp_src[:,:,int(h/2)-int(b/2):int(h/2)+int(b/2),int(w/2)-int(b/2):int(w/2)+int(b/2)] = amp_trg[:,:,int(h/2)-int(b/2):int(h/2)+int(b/2),int(w/2)-int(b/2):int(w/2)+int(b/2)]
+        
+        return_amp = amp_src
+       
+    if mask_type == 'circulair':
+        R = np.sqrt(beta_pixel/np.pi)
+        center_height = h // 2
+        center_width = w // 2
+        
+        # make circle mask
+        H, W = torch.meshgrid(torch.arange(h), torch.arange(w))
+        mask = ((H - center_height) ** 2 + (W - center_width) ** 2) <= R ** 2
+
+        # apply mask
+        return_amp = amp_trg * mask + amp_src * ~mask
+        
+    if mask_type == 'gauss':
+        # set constants for gauss mask
+        x, y = torch.meshgrid(torch.arange(h), torch.arange(w))
+        mu_x =  h // 2
+        mu_y = w // 2
+        sigma = np.sqrt(beta_pixel/np.pi)*1.5
+        norm_factor  =  1/((sigma**2) * 2 * np.pi)
+
+        # fill gauss mask
+        mask =  beta_pixel * norm_factor * (torch.exp(-(( (abs(x)-mu_x)**2 + (abs(y)-mu_y)**2)/ ( 2.0 * sigma**2 ) )))
+        # make the inverse of the gauss mask
+        invers_mask = torch.full((h,w), torch.max(torch.max(mask))) - mask
+        # aplly  masks and mistery factor of 4.5 to scale to same intesnety range as other FDA shapes
+        return_amp = (amp_trg * mask + amp_src * invers_mask) *4.5
+        
+    
+    if mask_type != 'circulair' and mask_type != 'gauss' and mask_type != 'square':
+        raise  Exception("mask_type not recognised, please use 'square', 'circulair' or 'gauss'")
+        
+          
+    return return_amp
+
+
+def shape_FDA_source_to_target(src_img, trg_img, beta=0.1, mask_type = 'circular'):
+    
+    img_ndim = src_img.dim()
+
+    # get fft of both source and target
+    # get fft of both source and target
+    fft_src = centered_fft(src_img.clone())
+    fft_trg = centered_fft(trg_img.clone())
+
+    
+    # Change from complex numbers to additional dimension for real and imaginary part
+    fft_src = torch.view_as_real(fft_src)
+    fft_trg = torch.view_as_real(fft_trg)
+
+    # extract amplitude and phase of both ffts
+    amp_src, pha_src = extract_ampl_phase( fft_src.clone())
+    amp_trg, pha_trg = extract_ampl_phase( fft_trg.clone())
+
+    # replace the low frequency amplitude part of source with that from target
+    if mask_type == 'square': 
+        amp_src_ = shape_low_freq_mutate( amp_src.clone(), amp_trg.clone(), beta, mask_type = 'square')
+    if mask_type == 'circulair': 
+        amp_src_ = shape_low_freq_mutate( amp_src.clone(), amp_trg.clone(), beta, mask_type = 'circulair')
+    if mask_type == 'gauss': 
+        amp_src_ = shape_low_freq_mutate( amp_src.clone(), amp_trg.clone(), beta, mask_type = 'gauss')
+    if mask_type != 'circulair' and mask_type != 'gauss' and mask_type != 'square':
+        raise  Exception("mask_type not recognised, please use 'square','circulair' or 'gauss'")
+    
+    # recompose fft of source
+    fft_src_ = torch.zeros( fft_src.size(), dtype=torch.float )
+    fft_src_[:,:,:,:,0] = torch.cos(pha_src.clone()) * amp_src_.clone()
+    fft_src_[:,:,:,:,1] = torch.sin(pha_src.clone()) * amp_src_.clone()
+
+    # Change back from additional dimension for real and imaginary part to complex numbers
+    fft_src_ = torch.view_as_complex(fft_src_)
+
+    # get the recomposed image: source content, target style
+    _, _, imgH, imgW = src_img.size()
+    src_in_trg = centered_ifft( fft_src_, s=(imgH,imgW) )
+    
+
+    # Convert to real-valued tensor
+    return torch.real(src_in_trg)
